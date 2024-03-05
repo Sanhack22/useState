b26a78aafdf159ec82948d02f98fe44f6483fe67
unknown <SenaTarde@ARMAPRRQDFSD224.FormacionSena.RED>
2024-03-05 13:58:46 -0500

    EndProyect

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,28 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
 import './App.css'
-
+let cont = 0
 function App() {
-  const [tasks, setTasks] = useState(
-    [
-      {
-        idTask: 1,
-        description: "Lavar Platos"
-      },
-      {
-        idTask: 2,
-        description: "Tender Cama"
-      }
-    ]
+  const  [tasks, setTasks] = useState([
+    {
+      idTask:1,
+      description:"Hoña"
+    }
+  ]
+   
 
   ) 
 
-const myReference = useRef('AMEE')
+const inputText = useRef('')
 
 const handleSetTask = () =>{
-  const newTask = TextTask.current.value;
-  console.log(newTask);
+    cont++
+    let newTask = {
+      idTask:cont,
+      description:inputText.current.value
+    }
+    setTasks([...tasks,newTask])
+
 
 }
 
@@ -32,17 +33,16 @@ const handleSetTask = () =>{
 
   return (
     <>
-      <input ref={myReference} id='taskInput' type="text" placeholder='Ingrese la tarea' />
-
+      <input ref={inputText} id='taskInput' type="text" placeholder='Ingrese la tarea' />
       <button onClick={handleSetTask} id='taskButton'>Crear Tarea</button>
       <hr />
       <div className='listTask'>
-        {tasks.map(x =>( 
-
+        {tasks.map(x =>(
           <h3 key={x.idTask}>{x.description}</h3>
-
         ))
+
         }
+        
       </div>
     </>
   )
