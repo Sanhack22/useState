8761dc37498589e652746b1f3b7aa8065c6aa336
unknown <SenaTarde@ARMAPRRQDFSD224.FormacionSena.RED>
2024-03-12 16:51:53 -0500

    ArchivePrueba

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useRef, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import {Boton} from './Components/Boton/Boton'
-import {NavBar} from './Components/NavBar/NavBar'
+import { Boton } from './Components/Boton/Boton'
+import { NavBar } from './Components/NavBar/NavBar'
 import './App.css'
 import { Main } from './Components/Layouts/Main/Main'
 import { CardTask } from './Components/CardTask/CardTask'
@@ -11,59 +11,81 @@ import { DivNewTask } from './Components/DivNewTask/DivNewTask'
 
 let cont = 0
 function App() {
-  const  [tasks, setTasks] = useState([
-    
+  const [tasks, setTasks] = useState([
+    {
+      idTask: cont,
+      name: "Aseo",
+      description: "Lavar Platos"
+    }
   ]
-   
+  )
+  const [tasksArchive, setArchive] = useState([
 
-  ) 
+  ])
 
-const inputText = useRef('')
-const inputTextName = useRef('')
+  const inputText = useRef('')
+  const inputTextName = useRef('')
 
 
-function cleanDiv() {
-  function cleanDiv() {
+
+
+  function cleanInputs() {
     inputText.current.value = ''
     inputTextName.current.value = ''
   }
-}
-
-
-const handleSetTask = () =>{
+  const handleSetTask = () => {
     cont++
     let newTask = {
-      idTask:cont,
-      name:inputTextName.current.value,
-      description:inputText.current.value
+      idTask: cont,
+      name: inputTextName.current.value,
+      description: inputText.current.value
     }
-    setTasks([...tasks,newTask])
-    cleanDiv()
+    setTasks([...tasks, newTask])
+    cleanInputs()
+  }
+
+  const archive = (id, name, description) => {
+    const taskToArchive = tasks.find(task => task.idTask === id);
 
 
+    if (taskToArchive) {
+      const taskToArchive = tasks.find(task => task.idTask === id);
 
-}
+      let newTaskArchive = {
+        idTask: taskToArchive.idTask,
+        name: taskToArchive.name,
+        description: taskToArchive.description
+      }
+
+      setArchive([...tasksArchive, newTaskArchive]);
+    }
+  }
   return (
     <>
-    
-      <Main>
-      <NavBar>
-      <Boton funcion={handleSetTask} id='' text='Ver Tareras Pendientes' />
-      <Boton funcion={handleSetTask} id='' text='Ver Tareas Archivadas' />
-      <Boton funcion={handleSetTask} id='' text='Ver Todas Las Tareas' />  
-      </NavBar>
-      <DivNewTask>
-        <input ref={inputTextName} id='taskInput' type="text"  placeholder='Nombre De La Tarea'/>
-        <textarea type="text"  ref={inputText} id='taskDescription'   placeholder='Ingrese La Descripcion'/>
-      </DivNewTask>
-      <Boton funcion={handleSetTask} id='taskButton' text='Crear Tarea' />
 
-      <div className='listTask'>
-        {tasks.map(x =>(
-          <CardTask key={cont} title={x.name} description={x.description} text='Archivaar'/>
-        ))
-        } 
-      </div>
+      <Main>
+        <NavBar>
+          <Boton funcion={handleSetTask} id='' text='Ver Tareras Pendientes' />
+          <Boton funcion={handleSetTask} id='' text='Ver Tareas Archivadas' />
+          <Boton funcion={handleSetTask} id='' text='Ver Todas Las Tareas' />
+        </NavBar>
+        <DivNewTask>
+          <input ref={inputTextName} id='taskInput' type="text" placeholder='Nombre De La Tarea' />
+          <textarea type="text" ref={inputText} id='taskDescription' placeholder='Ingrese La Descripcion' />
+        </DivNewTask>
+        <Boton funcion={handleSetTask} id='taskButton' text='Crear Tarea' />
+
+        <div className='listTask'>
+          {tasks.map(x => (
+        <CardTask onClick={() => archive(x.idTask, x.name, x.description)} key={x.idTask} title={x.name} description={x.description} text='Archivar' />
+    ))}
+
+        </div>
+         <div className='listTask'>
+        {tasksArchive.map(x => (
+            <CardTask key={x.idTask} title={x.name} description={x.description} text='Archivar' /*a={archive(x.idTask,x.name,x.description)}*/ />
+          ))}
+        </div>
       </Main>
     </>
 
--- src/Components/CardTask/CardTask.jsx
+++ src/Components/CardTask/CardTask.jsx
@@ -6,7 +6,7 @@ export const CardTask = (props) => {
     <div className='CardTask'>
       <div className='divTitle'><h3>{props.title}</h3></div>
       <div className='divDescription'><p>{props.description}</p></div>
-      <div onClick={props.funcion} className='divBoton'><button ref={props.refs}>{props.text}</button></div>
+      <div  className='divBoton'><button onClick={props.onClick} ref={props.refs}>{props.text}</button></div>
         
         
         
